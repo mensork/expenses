46897c075f64b6af262ccac5565f4819d6289978
mensork <mensork@ya.ru>
2017-03-16 19:33:23 +0300

    working ver 1.0

---
+++ my_expenses.rb
@@ -0,0 +1,46 @@
+#Файл для чтения .xml
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+require "date"
+require "rexml/document"
+
+current_path = File.dirname(__FILE__)
+file_name = current_path + "/my_expenses.xml"
+unless File.exist?(file_name)
+  abort "File not found"
+end
+file = File.new(file_name)
+doc = REXML::Document.new(file)
+
+day_costs = {}
+
+doc.elements.each("expenses/expense") do |item|
+  day_sum = item.attributes["amount"].to_i
+  date_sum = Date.parse(item.attributes["date"])
+  day_costs[date_sum] ||= 0
+  day_costs[date_sum] += day_sum
+end
+
+month_costs = {}
+
+day_costs.keys.each do |key|
+  month_costs[key.strftime("%B %Y")] ||= 0
+  month_costs[key.strftime("%B %Y")] += day_costs[key]
+end
+
+current_month = nil
+
+day_costs.keys.sort.each do |key|
+  if key.strftime("%B %Y") != current_month
+    current_month = key.strftime("%B %Y")
+    puts "\n\r#{current_month} -- expenses #{month_costs[current_month]}\n\r"
+  end
+  puts "#{key.day} day - #{day_costs[key]}"
+end
